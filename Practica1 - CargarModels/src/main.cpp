edb2c7c2f99bb1dc58b0ded984fc626de3b579c2
PauBlanes <paublanespardo@enti.cat>
2017-04-08 19:46:50 +0200

    Cambio entre modelos y reemplazar ficheros por otros para que vayan las librerias

--- Practica1 - CargarModels/src/main.cpp
+++ Practica1 - CargarModels/src/main.cpp
@@ -33,6 +33,9 @@ GLfloat Xrot;
 //inicializo camara
 Camera myCamera (vec3(0, 0, 3), vec3(0, 0, 0), 0.04f, 45.0f);
 
+//para escojer el modelo que cargar
+int whatToLoad = 1;
+
 int main() {
 	
 	//initGLFW
@@ -81,130 +84,10 @@ int main() {
 	Shader myShader ("./src/3DVertex.vertexshader", "./src/3DFragment.fragmentshader");	
 
 	//Modelos
-	Model modelo("./src/spider/spider.obj");
+	Model modelo1("./src/Models/spider/spider.obj");
+	Model modelo2("./src/Models/Toilet/Toilet.obj");
+	Model modelo3("./src/Models/tableandchair/tableandchair.obj");
 	
-	
-	//VAOVBOANTIGUOS
-	/*
-	GLfloat VertexBufferCube[] = {
-		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-		0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-	};
-	
-	vec3 CubesPositionBuffer[] = { //posiciones de los cubos
-		vec3(0.0f ,  0.0f,  0.0f),
-		vec3(2.0f ,  5.0f, -15.0f),
-		vec3(-1.5f, -2.2f, -2.5f),
-		vec3(-3.8f, -2.0f, -12.3f),
-		vec3(2.4f , -0.4f, -3.5f),
-		vec3(-1.7f,  3.0f, -7.5f),
-		vec3(1.3f , -2.0f, -2.5f),
-		vec3(1.5f ,  2.0f, -2.5f),
-		vec3(1.5f ,  0.2f, -1.5f),
-		vec3(-1.3f,  1.0f, -1.5f)
-	};
-			
-	// Crear los VBO, VAO y EBO	//Declarar el VBO y el EBO	
-	GLuint EBO, VBO, VAO;
-	//reservar memoria para el VAO, VBO y EBO
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
-	glGenVertexArrays(1, &VAO);
-
-	//Establecer el objeto
-	glBindVertexArray(VAO);{
-		//Enlazar el buffer con openGL
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(VertexBufferCube), VertexBufferCube, GL_STATIC_DRAW);
-
-		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-		//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(CubesPositionBuffer), CubesPositionBuffer, GL_STATIC_DRAW);
-
-		//Establecer las propiedades de los vertices
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (GLvoid*)0); 
-		glEnableVertexAttribArray(0);
-		//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GLfloat)));
-		//glEnableVertexAttribArray(1);
-		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-		glEnableVertexAttribArray(2);
-
-		//liberar el buffer
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		
-	} 
-	//liberar el buffer de vertices
-	glBindVertexArray(0);
-
-	//crear textura1
-	GLuint texture1ID;
-	glGenTextures(1, &texture1ID);
-	glBindTexture(GL_TEXTURE_2D, texture1ID);
-	//Wrapping y filtering
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	//imagen
-	int width, height;
-	unsigned char* image = SOIL_load_image("./src/texture.png", &width, &height, 0, SOIL_LOAD_RGB);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-	SOIL_free_image_data(image);
-	glBindTexture(GL_TEXTURE_2D, 0);
-	//crear textura2
-	GLuint texture2ID;
-	glGenTextures(1, &texture2ID);
-	glBindTexture(GL_TEXTURE_2D, texture2ID);
-	//Wrapping y filtering
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	//imagen
-	int width2, height2;
-	unsigned char* image2 = SOIL_load_image("./src/dietaSana.png", &width2, &height2, 0, SOIL_LOAD_RGB);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width2, height2, 0, GL_RGB, GL_UNSIGNED_BYTE, image2);
-	SOIL_free_image_data(image2);
-	glBindTexture(GL_TEXTURE_2D, 0);
-	*/	
 
 	//bucle de dibujado
 	while (!glfwWindowShouldClose(window))
@@ -248,7 +131,12 @@ int main() {
 		glUniformMatrix4fv(glGetUniformLocation(myShader.Program, "model"), 1, GL_FALSE, value_ptr(model));
 						
 		//Pintar
-		modelo.Draw(myShader, GL_FILL);
+		if (whatToLoad == 1)
+			modelo1.Draw(myShader, GL_FILL);
+		else if (whatToLoad == 2)
+			modelo2.Draw(myShader, GL_FILL);
+		else if (whatToLoad == 3)
+			modelo3.Draw(myShader, GL_FILL);
 
 		//eventos
 		glfwPollEvents();
@@ -267,7 +155,12 @@ int main() {
 }
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
-	
+	if (key == GLFW_KEY_1)
+		whatToLoad = 1;
+	if (key == GLFW_KEY_2)
+		whatToLoad = 2;
+	if (key == GLFW_KEY_3)
+		whatToLoad = 3;
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);	
 			
